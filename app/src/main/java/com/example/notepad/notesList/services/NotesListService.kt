408c82630ffa32bd55ef47270876ca07d8ba6271
szoczek <szymon.zoczek@euvic.pl>
2020-02-13 09:17:34 +0100

    delete/update note on server

--- app/src/main/java/com/example/notepad/notesList/services/NotesListService.kt
+++ app/src/main/java/com/example/notepad/notesList/services/NotesListService.kt
@@ -5,6 +5,9 @@ import com.example.notepad.db.NoteDatabase
 import com.example.notepad.db.models.Note
 import com.example.notepad.notesList.utils.NoteOperationResult
 import com.example.notepad.notesList.utils.NotesListOperationResult
+import com.example.notepad.service.NoteAPIService
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import io.reactivex.Observable
 import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -14,32 +17,33 @@ import java.lang.Exception
 
 class NotesListService(context: Context) {
     private val db by lazy { NoteDatabase.get(context).noteDao() }
+    private val api by lazy { NoteAPIService.getService() }
+    private val listType = object : TypeToken<List<Note>>() {}.type
+    private val noteType = object : TypeToken<Note>() {}.type
 
     fun deleteNote(
+        id: Int,
         note: Note
     ): Observable<NoteOperationResult> {
-        return Observable.fromCallable {
-            try {
-                val id = note.id
-                db.delete(note)
-                NoteOperationResult.Completed(id)
-            } catch (ex: Exception) {
-                NoteOperationResult.Failed(ex.toString())
-            }
-        }.subscribeOn(Schedulers.io())
+        return api.delete(id).map {
+            id
+        }.doOnNext { db.delete(note) }
+            .map { NoteOperationResult.Completed(it) as NoteOperationResult }
+            .subscribeOn(Schedulers.io())
+            .onErrorReturn { NoteOperationResult.Failed(it.message.toString()) }
     }
 
     fun updateNote(
         note: Note
     ): Observable<NoteOperationResult> {
-        return Observable.fromCallable {
-            try {
-                db.update(note)
-                NoteOperationResult.Completed(note.id)
-            } catch (ex: Exception) {
-                NoteOperationResult.Failed(ex.toString())
-            }
+        return api.update(note.id, note).map { json ->
+            Gson().fromJson(json, noteType) as Note
+        }.doOnNext { updatedNote ->
+            db.insert(updatedNote)
+        }.map { update ->
+            NoteOperationResult.Completed(update.id) as NoteOperationResult
         }.subscribeOn(Schedulers.io())
+            .onErrorReturn { NoteOperationResult.Failed(it.message.toString()) }
     }
 
     fun loadNotes(filter: String, limit: Int, skip: Int): Observable<NotesListOperationResult> {
@@ -48,29 +52,42 @@ class NotesListService(context: Context) {
                 if (filter.isNotEmpty() && filter.length < 3)
                     return@fromCallable NotesListOperationResult.Failed("Query must be longer than 2 characters")
 
-
-                val items: List<Note> = if (filter.isEmpty())
-                    db.allNotesOrderByDateLimitSkip(limit, 0)
-                else
+                val items: List<Note> =
                     db.allNotesFilterByTitleOrderByDateLimitSkip(filter, limit, skip)
+
+                if (items.isEmpty())
+                    throw Error("List is Empty")
 
                 NotesListOperationResult.Completed(items)
 
             } catch (ex: Exception) {
                 NotesListOperationResult.Failed(ex.toString())
             }
-        }.subscribeOn(Schedulers.io())
+        }.onErrorResumeNext(api.getNotes(limit, skip, "").map { json ->
+            val items: List<Note> = Gson().fromJson(json, listType)
+            db.insert(items)
+            NotesListOperationResult.Completed(items)
+        }).subscribeOn(Schedulers.io())
     }
 
     fun loadNotes(limit: Int, skip: Int): Observable<NotesListOperationResult> {
         return Observable.fromCallable {
             try {
                 val items = db.allNotesOrderByDateLimitSkip(limit, skip)
+                if (items.isEmpty())
+                    throw Error("List is Empty")
+
                 return@fromCallable NotesListOperationResult.Completed(items)
 
             } catch (ex: Exception) {
                 return@fromCallable NotesListOperationResult.Failed(ex.toString())
             }
-        }.subscribeOn(Schedulers.io())
+        }.onErrorResumeNext(
+            api.getNotes(limit, skip).map { json ->
+                val items: List<Note> = Gson().fromJson(json, listType)
+                db.insert(items)
+                NotesListOperationResult.Completed(items)
+            }).onErrorReturn { NotesListOperationResult.Failed(it.message.toString()) }
+            .subscribeOn(Schedulers.io())
     }
 }
